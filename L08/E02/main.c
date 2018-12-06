b4e5e146cafafabe168d8b91b064100523721799
Andrea Cognolato <andrecogno@hotmail.it>
2018-12-06 17:20:06 +0100

    Finish 8.2

--- L08/E02/main.c
+++ L08/E02/main.c
@@ -7,12 +7,27 @@ typedef struct {
   int value[2];
 } tile;
 
+tile rotate_tile(tile non_rotated) {
+  tile t;
+
+  t.value[0] = non_rotated.value[1];
+  t.color[0] = non_rotated.color[1];
+
+  t.value[1] = non_rotated.value[0];
+  t.color[1] = non_rotated.color[0];
+
+  return t;
+}
+
 typedef struct {
   int id;
   int rotated;
 } cell;
 
 typedef cell **board_t;
+
+static int max_score = 0;
+static tile **max_tb;
 
 void print_tiles(tile *tiles, size_t tiles_len) {
   for (size_t i = 0; i < tiles_len; i++) {
@@ -30,125 +45,149 @@ void print_board(board_t board, size_t rows, size_t cols) {
   }
 }
 
-void print_board_with_tiles(board_t board, size_t rows, size_t cols,
-                            tile *tiles) {
+void print_board_with_tiles(tile **tiles, size_t rows, size_t cols) {
   for (size_t i = 0; i < rows; i++) {
     for (size_t j = 0; j < cols; j++) {
-      int id = board[i][j].id;
-      if (id == -1) {
-        printf("[** **] ");
-      } else {
-        tile t = tiles[id];
-        int rotated = board[i][j].rotated;
-        if (rotated == 1) {
-          printf("[%c%d %c%d] ", t.color[1], t.value[1], t.color[0],
-                 t.value[0]);
-        } else {
-          printf("[%c%d %c%d] ", t.color[0], t.value[0], t.color[1],
-                 t.value[1]);
-        }
-      }
+      tile t = tiles[i][j];
+      printf("[%c%d %c%d] ", t.color[0], t.value[0], t.color[1], t.value[1]);
     }
     printf("\n");
   }
 }
 
-void check(size_t *sol, size_t sol_len, tile *tiles, size_t tiles_len,
-           board_t board, size_t rows, size_t cols) {
-
-  board_t new_board = calloc(rows, sizeof(cell *));
+void check(size_t *sol, size_t k, tile *tiles, size_t n, board_t board,
+           size_t rows, size_t cols) {
+  // Init board to test the solution on
+  tile **tb = calloc(rows, sizeof(tile *));
   for (size_t i = 0; i < rows; i++) {
-    new_board[i] = calloc(cols, sizeof(cell));
+    tb[i] = calloc(cols, sizeof(tile));
   }
 
-  size_t k = 0;
+  // Fill the board with tiles
+  size_t l = 0;
   for (size_t i = 0; i < rows; i++) {
     for (size_t j = 0; j < cols; j++) {
       cell c = board[i][j];
-      if (c.id == -1) {
-        c.id = (int)sol[k++];
-
-        // Not rotated if [0, tiles_len), rotated if [tiles_len, 2*tiles_len)
-        c.rotated = (k >= tiles_len);
-      }
-      new_board[i][j] = c;
-    }
-  }
-
-  // TODO: finish
-  printf("a\n");
-
-  // Check rows color
-  for (size_t i = 0; i < rows; i++) {
-    int id_first = new_board[i][0].id;
-    char color_first = tiles[id_first].color[0];
-
-    for (size_t j = 0; j < cols; j++) {
-      int id = new_board[i][j].id;
-
-      if (tiles[id].color[0] != color_first) {
-        return;
+      if (c.id != -1) {
+        if (c.rotated == 1) {
+          // if it's rotated go forward in the array to pick the rotated version
+          // of this tile
+          tb[i][j] = tiles[c.id + n / 2];
+        } else {
+          tb[i][j] = tiles[c.id];
+        }
+      } else {
+        tb[i][j] = tiles[sol[l++]];
       }
     }
   }
 
-  printf("\nThe *after* board\n");
-  print_board_with_tiles(new_board, rows, cols, tiles);
+  int score = 0;
 
-  // Check cols color
+  // Calculate row score
+  for (int i = 0; i < rows; ++i) {
+    bool same_color_row = true;
+    char first_color_row = tb[i][0].color[0];
 
+    int row_score = 0;
+    for (int j = 0; j < cols; ++j) {
+      if (tb[i][j].color[0] != first_color_row) {
+        same_color_row = false;
+      } else {
+        row_score += tb[i][j].value[0];
+      }
+    }
+    if (same_color_row) {
+      score += row_score;
+    }
+  }
+
+  // Calculate column score
+  for (int j = 0; j < cols; ++j) {
+    bool same_color_col = true;
+    char first_color_col = tb[0][j].color[1];
+
+    int col_score = 0;
+    for (int i = 0; i < rows; ++i) {
+      if (tb[i][j].color[1] != first_color_col) {
+        same_color_col = false;
+      } else {
+        col_score += tb[i][j].value[1];
+      }
+    }
+
+    if (same_color_col) {
+      score += col_score;
+    }
+  }
+
+  if (score > max_score) {
+    max_score = score;
+
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        max_tb[i][j] = tb[i][j];
+      }
+    }
+  }
   // Cleanup
   for (size_t i = 0; i < rows; i++) {
-    free(new_board[i]);
+    free(tb[i]);
   }
-  free(new_board);
+  free(tb);
 
-  // TODO: finish
-  // TODO: implement max value find
-  exit(EXIT_SUCCESS);
   return;
 }
 
-void dispositions(size_t pos, size_t sol_len, size_t *sol, bool *mark,
-                  tile *tiles, size_t tiles_len, board_t board, size_t rows,
-                  size_t cols) {
-  if (pos == sol_len) {
-    check(sol, sol_len, tiles, tiles_len, board, rows, cols);
+void dispositions(size_t pos, size_t k, size_t *sol, bool *mark, tile *tiles,
+                  size_t n, board_t board, size_t rows, size_t cols,
+                  cell *used_cells, size_t used_cells_len) {
+  if (pos == k) {
+    check(sol, k, tiles, n, board, rows, cols);
     return;
   }
 
-  for (size_t i = 0; i < tiles_len; i++) {
-    if (!mark[i]) {
+  for (size_t i = 0; i < n; i++) {
+    bool skip = false;
+    for (int j = 0; j < used_cells_len; ++j) {
+      if (i == used_cells[j].id) skip = true;
+    }
+    if (skip) continue;
+
+    // If neither the cell, neither its rotated version are used
+    if (!mark[i] && !mark[i + n / 2]) {
+      // mark both
       mark[i] = true;
+      mark[i + n / 2] = true;
+
       sol[pos] = i;
-      dispositions(pos + 1, sol_len, sol, mark, tiles, tiles_len, board, rows,
-                   cols);
+      dispositions(pos + 1, k, sol, mark, tiles, n, board, rows, cols,
+                   used_cells, used_cells_len);
+
+      // unmark both
       mark[i] = false;
+      mark[i + n / 2] = false;
     }
   }
 }
 
 void solve(tile *tiles, size_t tiles_len, int free_cells, board_t board,
-           size_t rows, size_t cols) {
-  // try all possible dispositions of tiles in the free cells
-  // assuming a tile can be rotated
-  // and find the disposition which yields the max score
-
-  size_t sol_len = 5;
+           size_t rows, size_t cols, cell *used_cells, size_t used_cells_len) {
+  // Init solution array
+  size_t sol_len = free_cells;
   size_t *sol = calloc(sol_len, sizeof(size_t));
-  bool *mark = calloc(sol_len, sizeof(bool));
 
+  // Init mark array used to make the simple dispositions of n in k
+  bool *mark = calloc(sol_len, sizeof(bool));
   for (size_t i = 0; i < sol_len; i++) {
     mark[i] = false;
   }
 
-  // Dispositions of all n tiles in the k free cells
-  size_t n = 2 * tiles_len;
-
-  // FIXME: not considering rotated tiles
+  size_t n = tiles_len;
   size_t k = free_cells;
 
-  dispositions(0, k, sol, mark, tiles, n, board, rows, cols);
+  dispositions(0, k, sol, mark, tiles, n, board, rows, cols, used_cells,
+               used_cells_len);
 }
 
 int main() {
@@ -163,34 +202,52 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
+  // Read the tiles and also create the rotated version
   size_t tiles_len = 0;
   fscanf(fp_tiles, "%zu ", &tiles_len);
 
+  // Double the tiles to have a handy reference to a rotated tile because
+  // in the solution we will have to try rotated tiles too
+  tiles_len *= 2;
   tile *tiles = calloc(tiles_len, sizeof(tile));
 
-  for (size_t i = 0; i < tiles_len; i++) {
+  for (size_t i = 0; i < (tiles_len / 2); i++) {
     tile t;
     fscanf(fp_tiles, "%c %d %c %d ", &(t.color[0]), &(t.value[0]),
            &(t.color[1]), &(t.value[1]));
     tiles[i] = t;
   }
+  for (size_t i = tiles_len / 2; i < tiles_len; i++) {
+    tile non_rotated = tiles[i - tiles_len / 2];
+    tiles[i] = rotate_tile(non_rotated);
+  }
 
+  // Read the board
   unsigned int rows, cols;
   fscanf(fp_board, "%d %d ", &rows, &cols);
-  board_t board = calloc(rows, sizeof(cell *));
 
+  board_t board = calloc(rows, sizeof(cell *));
   for (size_t i = 0; i < rows; i++) {
     board[i] = calloc(cols, sizeof(cell));
   }
+
+  cell *used_cells = calloc(tiles_len / 2, sizeof(cell));
+  size_t used_cells_len = 0;
 
   for (size_t i = 0; i < rows; i++) {
     for (size_t j = 0; j < cols; j++) {
       cell c;
       fscanf(fp_board, "%d/%d ", &c.id, &c.rotated);
+
+      if (c.id != -1) {
+        used_cells[used_cells_len++] = c;
+      }
+
       board[i][j] = c;
     }
   }
 
+  // Count the free cells
   int free_cells = 0;
   for (size_t i = 0; i < rows; i++) {
     for (size_t j = 0; j < cols; j++) {
@@ -199,9 +256,25 @@ int main() {
       }
     }
   }
-  solve(tiles, tiles_len, free_cells, board, rows, cols);
+
+  // TODO: free
+  max_tb = calloc(rows, sizeof(tile *));
+  for (size_t i = 0; i < rows; i++) {
+    max_tb[i] = calloc(cols, sizeof(tile));
+  }
+
+  // Solve by trying all tiles permutations in the free cells
+  solve(tiles, tiles_len, free_cells, board, rows, cols, used_cells,
+        used_cells_len);
+  printf("max_score %d\n", max_score);
+  print_board_with_tiles(max_tb, rows, cols);
 
   // Cleanup
+  free(used_cells);
+  for (size_t i = 0; i < rows; i++) {
+    free(max_tb[i]);
+  }
+  free(max_tb);
   for (size_t i = 0; i < rows; i++) {
     free(board[i]);
   }
