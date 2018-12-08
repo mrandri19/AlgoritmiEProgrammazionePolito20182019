d76ac0416cd11cd682ddef15b09c1269eb73c0c5
Andrea Cognolato <andrecogno@hotmail.it>
2018-12-08 18:04:04 +0100

    Finish 9.1

--- L09/E01/main.c
+++ L09/E01/main.c
@@ -1,42 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int pair_t[2];
+
 int max(int a, int b) { return ((a > b) ? a : b); }
 
-int duration(int S[][2], int S_len, int t) {
-  int best = 0;
-  for (int i = 0; i < t; ++i) {
-    if (S[i][1] == t) {
-      best = max(best, S[i][1] - S[i][0]);
+int duration(pair_t p) { return (p[1] - p[0]); }
+
+int opt(int i, pair_t S[], int S_len) {
+  int *solution = calloc(S_len, sizeof(int));
+
+  // Print start array
+  for (int n = 0; n < S_len; n++) {
+    printf("%d ", n);
+  }
+  printf("\n");
+  for (int n = 0; n < S_len; n++) {
+    printf("%d ", solution[n]);
+  }
+  printf("\n");
+
+  // Solve
+  solution[0] = duration(S[0]);
+  printf("\n");
+
+  for (int n = 1; n <= i; n++) {
+    int best = solution[n - 1];
+
+    for (int j = 0; j < n; j++) {
+      if (S[n][0] < S[j][1]) {
+        continue;
+      }
+      best = max(best, duration(S[n]) + solution[j]);
     }
+
+    solution[n] = best;
   }
-  return best;
+
+  // Print end array
+  for (int n = 0; n < S_len; n++) {
+    printf("%d ", n);
+  }
+  printf("\n");
+  for (int n = 0; n < S_len; n++) {
+    printf("%d ", solution[n]);
+  }
+  printf("\n");
+
+  free(solution);
+  return solution[S_len - 1];
 }
 
-int opt(int S[][2], int S_len, int t) {
-  if (t == 0) {
-    return 0;
-  }
-
-  int best = 0;
-  best = max(best, opt(S, S_len, t - 1));
-
-  for (int i = 0; i < t; ++i) {
-    // TODO: handle case where 2 events finish at the same time
-    best = max(best, opt(S, S_len, i) + duration(S, S_len, t));
-  }
-
-  return best;
-}
-
-int main(int argc, char const *argv[]) {
-  int S[][2] = {{1, 2}, {2, 4}, {2, 5}, {3, 5}, {5, 7}, {6, 8}};
+int main() {
+  pair_t S[] = {{1, 2}, {2, 4}, {2, 5}, {3, 5}, {5, 7}, {6, 8}};
   int S_len = sizeof(S) / sizeof(S[0]);
 
   // Bubble sort pairs on finish time
   for (int i = 0; i < S_len - 1; ++i) {
     for (int j = 0; j < S_len - i - 1; ++j) {
-      if (S[j][1] < S[j + 1][1]) {
+      if (S[j][1] > S[j + 1][1]) {
         int tmp[2];
         tmp[0] = S[j][0];
         tmp[1] = S[j][1];
@@ -50,9 +72,11 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  // find subset that maximises durations sum
-  int max_duration = opt(S, S_len, S[S_len - 1][1]);
-  printf("max_duration = %d\n", max_duration);
+  for (int i = 0; i < S_len; i++) {
+    printf("%d %d\n", S[i][0], S[i][1]);
+  }
+
+  printf("opt(%d) = %d\n", S_len, opt(S_len - 1, S, S_len));
 
   return 0;
 }
