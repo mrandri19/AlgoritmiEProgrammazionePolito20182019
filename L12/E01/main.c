280251b618173b46cee4ced4ab761fb4aa74a4ad
Andrea Cognolato <andrecogno@hotmail.it>
2019-01-15 18:53:58 +0100

    Finish Lab 12

--- L12/E01/main.c
+++ L12/E01/main.c
@@ -4,22 +4,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "priority_queue.h"
-
 #define VERTEX_LEN 30 + 1
+#define INFINITY 2147483647 // (Radio edit)
 
-int linear_search(char* needle, char** haystack, size_t len) {
+int linear_search(char *needle, char **haystack, size_t len) {
   for (size_t i = 0; i < len; ++i) {
-    if (strcmp(needle, haystack[i]) == 0) return i;
+    if (strcmp(needle, haystack[i]) == 0)
+      return (int)i;
   }
   return -1;
 }
 
 // j is an ancestor of i, it does this by checking if they have a common
 // ancestor
-bool is_ancestor(int j, int i, int* parent, size_t vertexes_len) {
-  while (parent[j] != j) j = parent[j];
-  while (parent[i] != i) i = parent[i];
+bool is_ancestor(int j, int i, int *parent) {
+  while (parent[j] != j)
+    j = parent[j];
+  while (parent[i] != i)
+    i = parent[i];
   return i == j;
 }
 
@@ -28,7 +30,7 @@ static bool has_cycle = false;
 
 // Perform dfs on graph G, starting from vertex i, populating the visited array
 // of length vertexes_len
-void dfs(int** G, size_t i, int* start, int* end, int* parent,
+void dfs(int **G, size_t i, int *start, int *end, int *parent,
          size_t vertexes_len) {
   start[i] = time++;
 
@@ -41,7 +43,7 @@ void dfs(int** G, size_t i, int* start, int* end, int* parent,
       } else {
         // If the vertex to visit has been discovered before than us and it's
         // our ancestor
-        if ((start[j] < start[i]) && is_ancestor(j, i, parent, vertexes_len)) {
+        if ((start[j] < start[i]) && is_ancestor(j, i, parent)) {
           has_cycle = true;
         }
       }
@@ -51,21 +53,21 @@ void dfs(int** G, size_t i, int* start, int* end, int* parent,
   end[i] = time++;
 }
 
-bool detect_cycles(int** G, char** vertexes, size_t vertexes_len) {
+bool detect_cycles(int **G, size_t vertexes_len) {
   // Create the start, end arrays to be populated in the DFS
-  int* start = calloc(vertexes_len, sizeof(int));
+  int *start = calloc(vertexes_len, sizeof(int));
   for (size_t i = 0; i < vertexes_len; ++i) {
     start[i] = -1;
   }
 
-  int* end = calloc(vertexes_len, sizeof(int));
+  int *end = calloc(vertexes_len, sizeof(int));
   for (size_t i = 0; i < vertexes_len; ++i) {
     end[i] = -1;
   }
 
-  int* parent = calloc(vertexes_len, sizeof(int));
+  int *parent = calloc(vertexes_len, sizeof(int));
   for (size_t i = 0; i < vertexes_len; ++i) {
-    parent[i] = i;
+    parent[i] = (int)i;
   }
 
   has_cycle = false;
@@ -90,8 +92,8 @@ typedef struct {
   int weight;
 } edge_t;
 
-void populate_matrix_from_edge_array(int** G, bool* solution,
-                                     size_t solution_len, edge_t* edges) {
+void populate_matrix_from_edge_array(int **G, bool *solution,
+                                     size_t solution_len, edge_t *edges) {
   for (size_t i = 0; i < solution_len; ++i) {
     if (solution[i]) {
       G[edges[i].a][edges[i].b] = edges[i].weight;
@@ -101,36 +103,38 @@ void populate_matrix_from_edge_array(int** G, bool* solution,
   }
 }
 
-int cardinality(bool* a, size_t len) {
+int cardinality(bool *a, size_t len) {
   int b = 0;
-  for (int i = 0; i < len; ++i) {
-    if (a[i]) b++;
+  for (size_t i = 0; i < len; ++i) {
+    if (a[i])
+      b++;
   }
   return b;
 }
 
-int weight(bool* solution, size_t len, edge_t* edges) {
+int weight(bool *solution, size_t len, edge_t *edges) {
   int total = 0;
 
   // If the i-th edge is in the solution then add its weight to the total
-  for (int i = 0; i < len; ++i) {
-    if (solution[i]) total += edges[i].weight;
+  for (size_t i = 0; i < len; ++i) {
+    if (solution[i])
+      total += edges[i].weight;
   }
   return total;
 }
 
-void powerset(size_t index, bool* solution, size_t solution_len,
-              bool* dag_edges_min_cardinality, bool* dag_edges_min_weight,
-              edge_t* edges, char** vertexes, size_t vertexes_len) {
+void powerset(size_t index, bool *solution, size_t solution_len,
+              bool *dag_edges_min_cardinality, bool *dag_edges_min_weight,
+              edge_t *edges, char **vertexes, size_t vertexes_len) {
   if (index == solution_len) {
-    int** G = calloc(vertexes_len, sizeof(int*));
+    int **G = calloc(vertexes_len, sizeof(int *));
     for (size_t i = 0; i < vertexes_len; ++i) {
       G[i] = calloc(vertexes_len, sizeof(int));
     }
 
     populate_matrix_from_edge_array(G, solution, solution_len, edges);
 
-    if (!detect_cycles(G, vertexes, vertexes_len)) {
+    if (!detect_cycles(G, vertexes_len)) {
       if (cardinality(solution, solution_len) >
           cardinality(dag_edges_min_cardinality, solution_len)) {
         // Copy the new best solution
@@ -165,44 +169,79 @@ void powerset(size_t index, bool* solution, size_t solution_len,
            dag_edges_min_weight, edges, vertexes, vertexes_len);
 }
 
-int* dijkstra(int** G, size_t vertexes_len, int source) {
-  int* dist = calloc(vertexes_len, sizeof(int));
-  int* prev = calloc(vertexes_len, sizeof(int));
+void swap(int *A, size_t a, size_t b) {
+  int tmp = A[a];
+  A[a] = A[b];
+  A[b] = tmp;
+}
 
-  dist[source] = 0;
+void find_max_distances_dag(int **G, size_t vertexes_len, int source,
+                            int **distances_, int **topological_order_) {
+  // Perform topological sort, then apply relaxation in that order
 
-  priority_queue_t PQ = priority_queue_new();
-
-  for (size_t v = 0; v < vertexes_len; ++v) {
-    if (v != source) {
-      dist[v] = 2147483647;
-    }
-    prev[v] = -1;
-
-    priority_queue_add_with_priority(&PQ, v, dist[v]);
+  // Variables init
+  int *start = malloc(vertexes_len * sizeof(int));
+  for (size_t i = 0; i < vertexes_len; i++) {
+    start[i] = -1;
+  }
+  int *end = calloc(vertexes_len, sizeof(int));
+  int *parent = calloc(vertexes_len, sizeof(int));
+  int *topological_order = calloc(vertexes_len, sizeof(int));
+  for (size_t i = 0; i < vertexes_len; i++) {
+    topological_order[i] = (int)i;
   }
 
-  while (priority_queue_size(PQ) > 0) {
-    int u = priority_queue_extract_min(PQ);
+  int *distances = calloc(vertexes_len, sizeof(int));
+  for (size_t i = 0; i < vertexes_len; i++) {
+    distances[i] = -1;
+  }
+  distances[source] = 0;
 
-    for (int v = 0; v < vertexes_len; ++v) {
-      int alt = dist[u] + G[u][v];
-      if (alt < dist[v]) {
-        dist[v] = alt;
-        prev[v] = u;
-        priority_queue_decrease_priority(&PQ, v, alt);
+  // Restore time since it's a global variable
+  time = 0;
+  // Apply DFS to all vertexes
+  for (size_t i = 0; i < vertexes_len; i++) {
+    if (start[i] == -1) {
+      dfs(G, i, start, end, parent, vertexes_len);
+    }
+  }
+
+  // Bubble sort :|
+  for (size_t i = 0; i < vertexes_len; i++) {
+    for (size_t j = 0; j < vertexes_len - i - 1; j++) {
+      if (end[j] < end[j + 1]) {
+        swap(end, j, j + 1);
+        swap(topological_order, j, j + 1);
       }
     }
   }
 
-  free(prev);
-  priority_queue_free(PQ);
-  return dist;
+  // For each vertex, in topological order
+  for (size_t i = 0; i < vertexes_len; i++) {
+    // For each of its edges
+    for (size_t j = 0; j < vertexes_len; j++) {
+      int edge_len = G[topological_order[i]][j];
+      if (edge_len != 0) {
+        int relaxed_distance = distances[topological_order[i]] + edge_len;
+        // Apply relaxation
+        if (distances[j] < relaxed_distance) {
+          distances[j] = relaxed_distance;
+        }
+      }
+    }
+  }
+  (*distances_) = distances;
+  (*topological_order_) = topological_order;
+
+  // Cleanup
+  free(parent);
+  free(end);
+  free(start);
 }
 
 int main() {
   // Read file
-  FILE* fp = fopen("grafo.txt", "r");
+  FILE *fp = fopen("grafo.txt", "r");
   if (fp == NULL) {
     perror("cannot open grafo.txt");
     exit(EXIT_FAILURE);
@@ -213,7 +252,7 @@ int main() {
   fscanf(fp, "%zu ", &vertexes_len);
 
   // Allocate vertex names vector
-  char** vertexes = calloc(vertexes_len, sizeof(char*));
+  char **vertexes = calloc(vertexes_len, sizeof(char *));
   for (size_t i = 0; i < vertexes_len; ++i) {
     vertexes[i] = calloc(VERTEX_LEN, sizeof(char));
   }
@@ -224,7 +263,7 @@ int main() {
   }
 
   // Allocate adjacency matrix
-  int** G = calloc(vertexes_len, sizeof(int*));
+  int **G = calloc(vertexes_len, sizeof(int *));
   for (size_t i = 0; i < vertexes_len; ++i) {
     G[i] = calloc(vertexes_len, sizeof(int));
   }
@@ -234,7 +273,7 @@ int main() {
   int weight;
 
   size_t edges_len = 0;
-  edge_t* edges = calloc(2 * vertexes_len, sizeof(edge_t));
+  edge_t *edges = calloc(2 * vertexes_len, sizeof(edge_t));
 
   while (fscanf(fp, "%s %s %d ", a, b, &weight) != EOF) {
     int a_index = linear_search(a, vertexes, vertexes_len);
@@ -248,7 +287,7 @@ int main() {
   }
 
   // Detect if there are any cycles in the graph
-  if (detect_cycles(G, vertexes, vertexes_len)) {
+  if (detect_cycles(G, vertexes_len)) {
     printf("There are cycles\n");
   } else {
     printf("There are no cycles\n");
@@ -299,13 +338,21 @@ int main() {
   }
 
   // Allocate all path from each vertex
-  int** paths = calloc(vertexes_len, sizeof(int*));
+  int **paths = calloc(vertexes_len, sizeof(int *));
   for (size_t i = 0; i < vertexes_len; ++i) {
     paths[i] = calloc(vertexes_len, sizeof(int));
   }
 
-  // Find all the distances between every node of the min cardinality DAG
-  int* distances = dijkstra(G, vertexes_len, 0);
+  // Find all the distances from the source node and every other node of the min
+  // weight DAG
+  int *distances;
+  int *topological_order;
+  find_max_distances_dag(G, vertexes_len, 0, &distances, &topological_order);
+
+  for (size_t i = 0; i < vertexes_len; i++) {
+    printf("%c: %d\n", topological_order[i] + 'a',
+           distances[topological_order[i]]);
+  }
   free(distances);
 
   // Cleanup
